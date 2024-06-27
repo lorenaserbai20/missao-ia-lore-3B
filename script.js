6a74754340d256140829e46800b7a32453a8c21f
lorenaserbai20 <130658109+lorenaserbai20@users.noreply.github.com>
2024-06-27 11:20:21 -0300

    pergunta

---
+++ script.js
@@ -0,0 +1,29 @@
+const caixaPricipal = document.queryselector (".caixa-principal"); 
+const caixaPerguntas = document.queryselector (".caixa-perguntas"); 
+const caixaAlternativas = document.queryselector (".caixa-alternativas"); 
+const caixaResultada = document.queryselector (".caixa-resultado"); 
+const textoResultado = document.queryselector (".texto-resultado"); 
+
+const perguntas =[
+    {
+        enunciado: "Pergunta 1" , 
+        alternativas:["Alternativa 1", "Alternativa 2"],
+    },
+    {
+        enunciado: "Pergunta 2" ,
+        alternativas:["Alternativa 1", "Alternativa 2"],
+    },
+    {
+        enunciado: "Pergunta 3" ,
+        alternativas:["Alternativa 1", "Alternativa 2"],
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPerguntas(){
+    perguntaAtual = perguntas [atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado; 
+}
+mostraPerguntas();
