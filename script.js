44a84829679454e9b057133eabe8867ef247398e
lorenaserbai20 <130658109+lorenaserbai20@users.noreply.github.com>
2024-06-28 10:48:40 -0300

    ,

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const textoResultado = document.queryselector (".texto-resultado");
 
 const perguntas =[
     {
-        enunciado: "Pergunta 1" , 
+        enunciado: "Pergunta 1", 
         alternativas:["Alternativa 1", "Alternativa 2"],
     },
     {
-        enunciado: "Pergunta 2" ,
+        enunciado: "Pergunta 2",
         alternativas:["Alternativa 1", "Alternativa 2"],
     },
     {
-        enunciado: "Pergunta 3" ,
+        enunciado: "Pergunta 3",
         alternativas:["Alternativa 1", "Alternativa 2"],
     },
 ];
