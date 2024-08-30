8b71447619d7182b80af7adaa6641e2c2f67eac6
lorenaserbai20 <130658109+lorenaserbai20@users.noreply.github.com>
2024-08-30 12:03:42 -0300

    ponto

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", ()=> respostaSelecionada(alternativa));/**/
+        botaoAlternativas.addEventListener("click", ()=> respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
