56aa917eff9e4fab7e20d033ed6f982ae0b6e8dd
lorenaserbai20 <130658109+lorenaserbai20@users.noreply.github.com>
2024-08-30 11:42:36 -0300

    espaço

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const perguntas = [
      {
       enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
       alternativas: [
+        
           {
               texto: "Isso é assustador!",
               afirmacao: "No início ficou com medo do que essa tecnologia pode fazer.",
